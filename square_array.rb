6f2be7fd06fe543183a740aa53e379325207dc13
Brian Donnoe <dunow33@hotmail.com>
2020-08-04 07:46:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,7 @@
 def square_array(array)
   squares = []
   
-  array.collect do |number|
+  array.each do |number|
     squares.push(number * number)
   end
-  
-  puts squares
 end
-
-arraySquares = [2, 3, 5, 9]
-
-square_array(arraySquares)
